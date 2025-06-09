bbb767b5cb140c21df8b735dfa5ce85892f424c5
PedroHenriquevs <pedrohenriquesousa1523@gmail.com>
2025-06-09 18:40:58 -0300

    Atualizações das classes Brinquedo, inicialização das classes Pessoa e Montanha_Russa

--- parque.py
+++ parque.py
@@ -7,6 +7,7 @@ class Parque:
         self._total_vendidos = 0.0
         self._data = datetime.now()
         self.brinquedos = []
+        self._funcionarios = {}
 
     @property
     def total_ingresso(self):
@@ -25,7 +26,16 @@ class Parque:
         if (valor >= 0):
             self._total_vendidos += valor
         
+
+class Pessoa(ABC):
+    def __init__(self, nome, cpf, telefone):
+        self._nome = nome
+        self._cpf = cpf
+        self._telefone = telefone
     
+    
+    
+
 class Brinquedo(ABC):
     
     def __init__(self, id_brinquedo, capacidade, qtd_pessoa, altura_min):
@@ -52,14 +62,41 @@ class Brinquedo(ABC):
     def altura_min(self):
         return self._altura_min
     
+    @capacidade.setter
+    def capacidade(self, nova_capacidade):
+        if(nova_capacidade >= 0):
+            self._capacidade = nova_capacidade
+        else:
+            print("Erro: capacidade inválida")        
+
+    @qtd_pessoa.setter
+    def qtd_pessoa(self, qtd):
+        if(qtd >= 0):
+            self._qtd_pessoa = qtd
+        else:
+            print("Error: Quantidade inválida")
+
+    @altura_min.setter
+    def altura_min(self, altura):
+        if(altura >= 0):
+            self._altura_min = altura
+        else:
+            print("Error: Altura inválida!")
     
 
     def adicionar_pessoa(self):
         pass
 
-    def verifica_brinquedo
+    def mostrar_brinquedo(self):
+        pass
 
-        
+
+class Montanha_Russa(Brinquedo):
+    def __init__(self, id_brinquedo, capacidade, qtd_pessoa, altura_min):
+        super().__init__(id_brinquedo, capacidade, qtd_pessoa, altura_min)
+        self._qtd_ingresso = 0
+        self._total_venda = 0.0        
+
 
     
         
